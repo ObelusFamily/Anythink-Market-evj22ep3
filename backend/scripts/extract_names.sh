5bfb1e6682c4cd2eef70f61cc39852f1c60a2173
TATHAGATA ROY <98920199+roy9495@users.noreply.github.com>
2023-01-09 18:52:23 +0000

    bash script to find amazon users

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/bash
-grep -i "@amazon.com" $1 | cut -d ',' -f 3,2 | tr ',' ' ' > output_name.txt
+#!/bin/bash
+grep -i "@amazon.com" $1 | awk '{print $3,$2}' | tr "," " " > output_names.txt
