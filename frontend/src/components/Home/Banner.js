6b6b5d369ce017ebe7f1fa4e5728d352c39daa5d
vanessa-cooper-1 <ness+1@anythink.family>
2023-01-23 18:44:04 +0000

    Hooks of Change

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,25 +1,18 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
 
-const Wrapper = styled.section`
-  background: ${(darktheme) => darktheme.theme.styles.main}; ;
-
-`;
-
 const Banner = () => {
   return (
-    <Wrapper>
-      <div className="banner text-white">
-        <div className="container p-4 text-center">
-          <img src={logo} alt="banner" />
-          <div>
-            <span>A place to </span>
-            <span id="get-part">get</span>
-            <span> the cool stuff.</span>
-          </div>
+    <div className="banner text-white">
+      <div className="container p-4 text-center">
+        <img src={logo} alt="banner" />
+        <div>
+          <span>A place to </span>
+          <span id="get-part">get</span>
+          <span> the cool stuff.</span>
         </div>
       </div>
-    </Wrapper>
+    </div>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import React from "react";
 import { store, history } from "./store";
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from "src/styles/GlobalStyle.js";
+
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
 
@@ -13,12 +12,9 @@ import App from "./components/App";
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <ThemeProvider>
-        <GlobalStyle whiteColor />
-          <Switch>
-            <Route path="/" component={App} />
-          </Switch>
-      </ThemeProvider>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
     </ConnectedRouter>
   </Provider>,
 
